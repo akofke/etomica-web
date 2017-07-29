cdf77b9fe8f39358d4bc598ac8a0b0077c732d4d
Alex Kofke <akofke@gmail.com>
2017-07-29 13:27:35 -0400

    working demo i think

---
+++ src/components/Unused.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+export const Unused = () => <h1>findme</h1>
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+
+import { Hello } from './components/hello';
+import { Unused } from './components/unused';
+
+ReactDOM.render(
+    // <Hello compiler="ts" framework="react" />,
+    <Unused />,
+    document.getElementById("example")
+)
