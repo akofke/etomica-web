a2263ff828fc1fe5e0f70113245c8aa23cd664b1
Alex Kofke <akofke@gmail.com>
2017-08-31 01:51:32 -0400

    Draw box using boundary edges and tube meshes
    Tubes instead of lines in order to have width.

--- src/lib/Simulation3D.ts
+++ src/lib/Simulation3D.ts
@@ -1,6 +1,6 @@
 import {
     Engine, Scene, FreeCamera, Light, Vector3, HemisphericLight, MeshBuilder, ArcRotateCamera,
-    Camera
+    Camera, AnaglyphArcRotateCamera, Mesh, PickingInfo, LinesMesh, StandardMaterial, Color3
 } from "babylonjs";
 
 export class Simulation3D {
@@ -10,7 +10,8 @@ export class Simulation3D {
     private camera: Camera;
     private light: Light;
 
-    private atomMeshes: any[];
+    private atomMeshes: Mesh[] = [];
+    private edgeMeshes: Mesh[] = [];
 
     constructor(mountElement: HTMLCanvasElement) {
         this.element = mountElement;
@@ -19,12 +20,13 @@ export class Simulation3D {
 
         // this.camera = new FreeCamera("camera1", new Vector3(0, 5, -10), this.scene);
         this.camera = new ArcRotateCamera("ArcRotateCamera", 1, 0.8, 10, new Vector3(0, 0, 0), this.scene);
+        // this.camera = new AnaglyphArcRotateCamera("aar_cam", -Math.PI/2, Math.PI/4, 20, Vector3.Zero(), 0.033, this.scene);
+
 
         this.scene.activeCamera = this.camera;
         this.camera.attachControl(this.element, false);
 
-        this.light = new HemisphericLight('light1', new Vector3(0, 1, 0), this.scene);
-        this.atomMeshes = [];
+        this.light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
         // const sphere = MeshBuilder.CreateSphere('sphere1', {segments: 16, diameter: 2}, this.scene);
         // sphere.position.y = 1;
 
@@ -46,6 +48,26 @@ export class Simulation3D {
             atomMesh.position.z = positions[2];
             this.atomMeshes.push(atomMesh);
         });
+
+        const boxEdgeMat = new StandardMaterial("boxEdgeMat", this.scene);
+        boxEdgeMat.alpha = 1;
+        boxEdgeMat.diffuseColor = new Color3(0.9, 0.1, 0.1);
+        const edges: any[] = model["#box"][0]["#boundary"]["#shape"]["#edges"];
+        edges.forEach((edge) => {
+            const edgeVertices: any[] = edge["#vertices"];
+            // const edgeMesh = Mesh.CreateLines("box", edgeVertices.map((v) => new Vector3(v[0], v[1], v[2])), this.scene);
+            const edgeMesh = Mesh.CreateTube(
+                `box`,
+                edgeVertices.map((v) => new Vector3(v[0], v[1], v[2])),
+                1,
+                8,
+                () => 0.01,
+                Mesh.CAP_ALL,
+                this.scene
+            );
+            edgeMesh.material = boxEdgeMat;
+            this.edgeMeshes.push(edgeMesh);
+        });
     }
 
     public updatePositions(coords: number[][][]) {
@@ -58,7 +80,7 @@ export class Simulation3D {
         });
     }
 
-    public pickCoordinates(x: number, y: number): any {
+    public pickCoordinates(x: number, y: number): PickingInfo {
         return this.scene.pick(x, y);
     }
 }
