2111c822f20e72367f86c5678af0c5111bfdabc8
Alex Kofke <akofke@gmail.com>
2017-10-21 14:19:55 -0400

    Fix some of the bugs with Simulation index view
    Correctly detect if clicked node is leaf node. Only re-render
    the tree if search results prop changes. Add cool icons.

--- src/components/simulationIndex/SimulationIndexTree.tsx
+++ src/components/simulationIndex/SimulationIndexTree.tsx
@@ -8,13 +8,22 @@ export interface ISimulationIndexTreeProps {
     onSelect: (selected: SimClassInfo) => void;
 }
 
+interface ISimulationIndexTreeState {
+    nodes: ITreeNode[];
+
+}
+
+const arraysAreEqual = (arr1: string[], arr2: string[]) => {
+    return arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]);
+}
+
 export class SimulationIndexTree extends React.Component<ISimulationIndexTreeProps, any> {
-    private nodeInfoMap: { [nodeId: number]: SimClassInfo } = {};
+    private nodeInfoMap: { [nodeId: string]: SimClassInfo } = {};
 
     constructor(props: ISimulationIndexTreeProps) {
         super(props);
         this.state = {
-            nodes: this.getChildNodes(this.props.classTree),
+            nodes: this.getChildNodes(this.props.classTree, ""),
         };
     }
 
@@ -34,10 +43,13 @@ export class SimulationIndexTree extends React.Component<ISimulationIndexTreePro
     }
 
     public componentWillReceiveProps(nextProps: ISimulationIndexTreeProps) {
-        this.setState({
-            nodes: this.getChildNodes(nextProps.classTree)
-        });
+        if(!arraysAreEqual(this.props.searchResults, nextProps.searchResults)) {
+            this.setState({
+                nodes: this.getChildNodes(nextProps.classTree, "")
+            });
+        }
     }
+
 
     private handleNodeCollapse = (nodeData: ITreeNode) => {
         nodeData.isExpanded = false;
@@ -50,35 +62,40 @@ export class SimulationIndexTree extends React.Component<ISimulationIndexTreePro
     };
 
     private onNodeClick = (nodeData: ITreeNode) => {
-        if (!nodeData.hasCaret) {
+        if (!nodeData.childNodes) {
+            console.log(nodeData);
             if (this.state.selectedNode) {
                 this.state.selectedNode.isSelected = false;
             }
             nodeData.isSelected = true;
             this.setState({
-                selectedNode: nodeData
+                selectedNode: nodeData,
+                nodes: this.state.nodes
             });
             this.props.onSelect(this.nodeInfoMap[nodeData.id as number]);
         }
 
     }
 
-    private getChildNodes = (treeObj: PackageHierarchy): ITreeNode[] => {
+    private getChildNodes = (treeObj: PackageHierarchy, path: string): ITreeNode[] => {
         const childNodes: ITreeNode[] = [];
         treeObj.classes.forEach((classInfo) => {
-            const id = Math.random();
+            const id = classInfo.className;
             childNodes.push({
                 label: classInfo.className.split(".").pop(),
+                iconName: "graph",
                 id
             } as ITreeNode);
             this.nodeInfoMap[id] = classInfo;
 
         });
         Object.keys(treeObj.subpackages).forEach((key) => {
+            const currentPath = path + key + ".";
             childNodes.push({
                 label: key,
-                childNodes: this.getChildNodes(treeObj.subpackages[key]),
-                id: Math.random(),
+                iconName: "box",
+                childNodes: this.getChildNodes(treeObj.subpackages[key], currentPath),
+                id: currentPath,
                 isExpanded: this.props.searchResults.some(
                     // TODO: inefficient
                     (searchClassName) => searchClassName.split(".").indexOf(key) > -1
