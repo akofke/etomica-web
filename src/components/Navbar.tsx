730c046c1f144edc1a76578e09bcec842cd5f87e
Alex Kofke <akofke@gmail.com>
2017-10-24 21:54:02 -0400

    Fix element id, add home link to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import {Route} from "react-router";
 import {SimStatusControls} from "./SimStatusControls";
+import {Link} from "react-router-dom";
+import {Button} from "@blueprintjs/core";
 
 export const Navbar = () => {
     return(
         <nav className="pt-navbar" id="navbar">
             <div className="pt-navbar-group pt-align-left">
-                <div className="pt-navbar-heading">Etomica</div>
+                <div className="pt-navbar-heading">
+                    <Link to={"/"}>
+                        <Button text="Etomica" className="pt-minimal pt-large"/>
+                    </Link>
+                </div>
             </div>
             <div className="pt-navbar-group pt-align-right">
                <Route path={"/view/:simId"} render={renderControls}/>
--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,5 @@ import { App } from "./App";
 
 ReactDOM.render(
         <App/>,
-    document.getElementById("example"),
+    document.getElementById("App"),
 );
